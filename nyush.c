859a2a2909554c4a525a9ff6778ce9e590636f1c
YiLin Choi <yilinchoi@hotmail.com>
2026-02-19 23:34:57 -0500

    m3

--- nyush.c
+++ nyush.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 static void print_prompt(void)
 {
@@ -44,6 +46,34 @@ int main(void)
 
         if (line[0] == '\0') {
             continue;
+        }
+
+        /* Resolve the program path:
+         *   - starts with '/' -> absolute path, use as-is
+         *   - contains '/'   -> relative path, use as-is
+         *   - otherwise       -> prepend /usr/bin/
+         */
+        char prog[1024];
+        if (line[0] == '/' || strchr(line, '/') != NULL) {
+            snprintf(prog, sizeof(prog), "%s", line);
+        } else {
+            snprintf(prog, sizeof(prog), "/usr/bin/%s", line);
+        }
+
+        pid_t pid = fork();
+        if (pid < 0) {
+            perror("fork");
+            continue;
+        } else if (pid == 0) {
+            /* Child: exec the program (no arguments yet). */
+            char *argv[] = {prog, NULL};
+            execv(prog, argv);
+            fprintf(stderr, "Error: invalid program\n");
+            _exit(1);
+        } else {
+            /* Parent: wait for the child to finish. */
+            int status;
+            waitpid(pid, &status, 0);
         }
 
     }
